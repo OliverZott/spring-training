e3375578bd07fc92d1871fd90bcd5fd14622693a
Oliver Zott <zott_oliver@web.de>
2023-08-28 18:04:07 +0200

    IoC / DI - Bean Lifecycle methods

--- 02_core/src/main/java/org/velosaurus/core/rest/ScubaTraining.java
+++ 02_core/src/main/java/org/velosaurus/core/rest/ScubaTraining.java
@@ -1,5 +1,7 @@
 package org.velosaurus.core.rest;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -12,6 +14,16 @@ public class ScubaTraining implements Training {
 
     public ScubaTraining() {
         System.out.println("Constructor called: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doStartupStuff() {
+        System.out.println("Bean Lifecycle Startup method called in " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doDCleanupStuff() {
+        System.out.println("Bean Lifecycle Cleanup method called in " + getClass().getSimpleName());
     }
 
     @Override
