04f8b587c09dd8dd7a1e1c74b9367e4ad43e8e79
Oliver Zott <zott_oliver@web.de>
2023-08-28 17:56:54 +0200

    IoC / DI - Bean Scopes - Singleton

--- 02_core/src/main/java/org/velosaurus/core/rest/ScubaTraining.java
+++ 02_core/src/main/java/org/velosaurus/core/rest/ScubaTraining.java
@@ -1,10 +1,13 @@
 package org.velosaurus.core.rest;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class ScubaTraining implements Training {
 
     public ScubaTraining() {
--- 02_core/src/main/java/org/velosaurus/core/rest/TrainingController.java
+++ 02_core/src/main/java/org/velosaurus/core/rest/TrainingController.java
@@ -10,13 +10,15 @@ import org.velosaurus.utils.Gear;
 public class TrainingController {
 
     private final Training training;
+    private final Training secondTraining;
     private final Gear gear;
 
 
     @Autowired
-    public TrainingController(@Qualifier("scubaTraining") Training training, Gear gear) {
+    public TrainingController(@Qualifier("scubaTraining") Training training, @Qualifier("scubaTraining") Training secondTraining, Gear gear) {
         System.out.println("Constructor called: " + getClass().getSimpleName());
 
+        this.secondTraining = secondTraining;
         this.training = training;
         this.gear = gear;
     }
@@ -30,5 +32,18 @@ public class TrainingController {
     @GetMapping("gear")
     public String GetGear() {
         return gear.ShowGear().toString();
+    }
+
+    @GetMapping("/check")
+    public String CheckScope() {
+        var trainingHashCode = System.identityHashCode(training);
+        var trainingHashCode2 = System.identityHashCode(secondTraining);
+        boolean result = trainingHashCode == trainingHashCode2;
+        return "Comparing Training instances hash codes: "
+                + trainingHashCode
+                + " == "
+                + trainingHashCode2
+                + " is "
+                + result;
     }
 }
